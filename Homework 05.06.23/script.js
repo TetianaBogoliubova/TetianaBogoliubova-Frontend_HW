a00e23f812df94f7bd98d1698b82c6b5af9b34d3
Bogoliubova Tetiana <bogoltatyana26@gmail.com>
2023-06-05 17:43:33 +0200

    Homework_05.06

---
+++ Homework 05.06.23/script.js
@@ -0,0 +1,32 @@
+// const number = +prompt("Введите число");
+// const rezultat = number *  .1;
+// console.log(rezultat);
+
+
+// const number1 = prompt("Введите первое число");
+// const number2 = prompt("Введите второе число");
+// if(number1 < number2) {
+//     console.log(number2); 
+// } else {
+//     console.log(number1);
+// }
+
+
+// const number = +prompt("Введите число");
+// if(number < 100) {
+//     console.log("Число меньше 100")
+// } else if(number > 100) {
+//     console.log("Число больше 100")
+// }
+// else {
+//     console.log("Число равно 100")
+// }
+
+
+// const username = prompt("Введите свое имя");
+// const userage = prompt("Введите свой возраст");
+// if(userage > 18) {
+//     console.log(`Hello, ${username}`)
+// } else {
+//     console.log(`Hi, ${username}`)
+// }
